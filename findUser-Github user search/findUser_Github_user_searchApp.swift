01f46ae6cf2b9526162f21d228860455b84bfbad
h-yunwon <h_ywon@naver.com>
2023-07-12 12:08:55 +0900

    git init

---
+++ findUser-Github user search/findUser_Github_user_searchApp.swift
@@ -0,0 +1,17 @@
+//
+//  findUser_Github_user_searchApp.swift
+//  findUser-Github user search
+//
+//  Created by Yunwon Han on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct findUser_Github_user_searchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
